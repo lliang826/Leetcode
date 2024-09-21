8daac622f3e6a46fed8597782c933b2b939ae2b8
Lawrence Liang <lawrence.liang@hotmail.ca>
2024-09-20 18:23:47 -0700

    Finished problem 226

---
+++ 226-Invert_binary_tree.py
@@ -0,0 +1,25 @@
+'''
+Given the root of a binary tree, invert the tree, and return its root.
+'''
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+
+        # DFS post-order
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
