5067d5d0b1957ba1957c66dd9f031a4e00e3082f
Lawrence Liang <lawrence.liang@hotmail.ca>
2024-09-21 22:17:50 -0700

    Updated 141 Linked list cycle

--- 141-Linked_list_cycle.py
+++ 141-Linked_list_cycle.py
@@ -6,7 +6,7 @@ the index of the node that tail's next pointer is connected to. Note that pos is
 passed as a parameter.
 Return true if there is a cycle in the linked list. Otherwise, return false.
 '''
-from typing import List
+from typing import Optional
 
 
 class ListNode:
@@ -18,27 +18,59 @@ class ListNode:
 Floyd's tortoise and hare algorithm
 Analogy: imagine 2 runners on a track oval (which has a cycle). One of the runners is
 faster than the other. Assuming that they maintain their speeds for the entire duration,
-the fast runner will eventually meet the slow runner again.
+the fast runner will eventually overlap and meet the slow runner again.
 Time: O(n)
 Space: O(1)
 '''
-def linkedListCycle(head: List[ListNode]):
-    fast = head[0]
-    slow = head[0]
 
-    while fast and fast.next:
-        fast = fast.next.next
-        slow = slow.next
-        if fast == slow:
-            return True
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+
+        return False
         
-    return False
 
-node4 = ListNode(-4)
-node3 = ListNode(0, node4)
-node2 = ListNode(2, node3)
-node1 = ListNode(3, node2)
-node4.next = node1
+'''
+Test cases:
 
-list1 = [node1, node2, node3, node4]
-print(linkedListCycle(list1))
+head = [3,2,0,-4]
+pos = 1
+Output: true
+
+head = [1,2]
+pos = 0
+Output: true
+
+head = [1]
+pos = -1
+Output: false
+'''
+        
+
+# def linkedListCycle(head: List[ListNode]):
+#     fast = head[0]
+#     slow = head[0]
+
+#     while fast and fast.next:
+#         fast = fast.next.next
+#         slow = slow.next
+#         if fast == slow:
+#             return True
+        
+#     return False
+
+# node4 = ListNode(-4)
+# node3 = ListNode(0, node4)
+# node2 = ListNode(2, node3)
+# node1 = ListNode(3, node2)
+# node4.next = node1
+
+# list1 = [node1, node2, node3, node4]
+# print(linkedListCycle(list1))
