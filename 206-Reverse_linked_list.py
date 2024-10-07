6890ca7902bbf11a367acc4fda7cac61a4ad2a25
Lawrence Liang <lawrence.liang@hotmail.ca>
2024-10-06 22:38:58 -0700

    Added explanation for 206. Reverse Linked List

--- 206-Reverse_linked_list.py
+++ 206-Reverse_linked_list.py
@@ -13,6 +13,18 @@ class ListNode:
         self.val = val
         self.next = next
 
+'''
+Using pen and paper makes this iterative solution easier to understand.
+
+There are 2 pointers required: 'prev' tracks the previous node, and 'temp' tracks the next. In each
+iteration, 'temp' is used to track the next node, while the pointer for head.next is moved to 'prev'.
+Then, we move the prev and head pointers up by one. The important thing to remember is to return
+prev, not head. Since the last node in the list is pointing to null, when the loop completes, head
+will be pointing to null too. 'prev' will be pointing to the last node in the list.
+
+Time: O(n) - need to iterate through the entire linked list
+Space: O(1) - no additional data structures
+'''
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
