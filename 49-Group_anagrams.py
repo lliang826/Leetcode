5a0af4ca79fe21ff8c5fea4e8a87cfa93581a669
Lawrence Liang <lawrence.liang@hotmail.ca>
2024-10-13 12:27:44 -0700

    Added comment for 49. Group anagrams

--- 49-Group_anagrams.py
+++ 49-Group_anagrams.py
@@ -4,6 +4,11 @@
 Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, using all the original letters exactly once.
+
+Constraints:
+    - 1 <= strs.length <= 104
+    - 0 <= strs[i].length <= 100
+    - strs[i] consists of lowercase English letters.
 '''
 
 from collections import defaultdict
@@ -11,11 +16,32 @@ from typing import List
 
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # key: value
-        # # of letter occurences: [str]
-        
-        
+    '''
+    This solution requires a hashmap (or dictionary in Python), where the key:value pair is [# of letter occurences]: [string].
+    The tricky part of this problem is determing which strings are anagrams of each other. This problem is very similar to 
+    242. Valid anagram, but in problem 242, I used a hashmap for the solution. Initially, I tried to come up with a nested
+    hashmap solution, but something like that problably wouldn't work.
+
+    A smarter solution is to use an array to keep track of the letters. Anagrams can be determined by matching all the letters 
+    in the strings, including duplicates, but this can be done by creating an array of 26 zeroes, where each zero represents one
+    of the 26 letters in the alphabet according to the array index. When iterating through the characters in the string, the integer 
+    value at each index can be incremented to represent the corresponding letter. 
+
+    Once a string has been converted to its integer array representation, it can be stored in the hashmap; anagrams will be
+    grouped together so the result can be found by returning the dictionary.values() function. 
+
+    Some other things to note:
+        - the ord() function in Python returns the decimal integer Unicode/Ascii representation of a character, and subtracting
+        ord(char) by ord('a') returns the index (e.g., 'a' - 'a' = 97 - 97 = 0)
+        - defaultdict() is very important/useful to know: it's a subclass of the dictionary class and it works the same way, except
+        that it provides a default value for keys that do not exist. The argument passed in could be "int", "str", "list", or a 
+        lambda function
+            foo = defaultdict(int)
+            print(foo["apple"])
+        - In Python, dictionary keys cannot be mutable. Lists are mutable, but tuples are immutable, so the integer array has to be
+        cast as a tuple in order for the code to work
+    '''
+    def groupAnagrams1(self, strs: List[str]) -> List[List[str]]:
         dict = defaultdict(list)
 
         for str in strs:
@@ -26,18 +52,18 @@ class Solution:
 
             dict[tuple(chars)].append(str)
 
-        return dict.values()
+        return list(dict.values())
     
 solution = Solution()
 
 input1 = ["eat","tea","tan","ate","nat","bat"]
-print(list(solution.groupAnagrams(input1)))
+print(solution.groupAnagrams1(input1))
 
 input2 = [""]
-print(list(solution.groupAnagrams(input2)))
+print(solution.groupAnagrams1(input2))
 
 input3 = ["a"]
-print(list(solution.groupAnagrams(input3)))
+print(solution.groupAnagrams1(input3))
 
 input4 = ["ddddddddddg","dgggggggggg"]
-print(list(solution.groupAnagrams(input4)))
+print(solution.groupAnagrams1(input4))
